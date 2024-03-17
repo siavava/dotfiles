aee8b71d5e328a7f82696e2c5c3ee013b933d888
siavava <amittaijoel@outlook.com>
2024-03-17 06:35:53 -0400

    (update): conda setup

--- exports.fish
+++ exports.fish
@@ -14,3 +14,16 @@ export PATH="$BUN_INSTALL/bin:$PATH"
 export NVM_DIR="$HOME/.nvm"
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+if test -f /opt/homebrew/anaconda3/bin/conda
+  eval /opt/homebrew/anaconda3/bin/conda "shell.fish" "hook" $argv | source
+else
+  if test -f "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
+      . "/opt/homebrew/anaconda3/etc/fish/conf.d/conda.fish"
+  else
+      set -x PATH "/opt/homebrew/anaconda3/bin" $PATH
+  end
+end
+# <<< conda initialize <<<
