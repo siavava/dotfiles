3e339c8ef1b810f83db98d45737996fc07960e94
siavava <amittaijoel@outlook.com>
2024-03-28 01:57:04 -0400

    (update): tex script, new alias

--- alias.fish
+++ alias.fish
@@ -18,3 +18,6 @@ alias lt='ls -s -F -h -1'
 
 # search previous commands
 alias search='history | grep'
+
+# redeploy blog
+alias redeploy='curl -X POST https://api.netlify.com/build_hooks/658d9d835e2c050092fdfe38'
