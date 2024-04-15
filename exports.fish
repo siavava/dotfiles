7aee06249f1cd593ff4d9a5db4511f9453f2b1b4
siavava <amittaijoel@outlook.com>
2024-04-15 17:20:16 -0400

    (fix): conda init error

--- exports.fish
+++ exports.fish
@@ -15,6 +15,10 @@ export NVM_DIR="$HOME/.nvm"
 #[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 # [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+# vscode error fix
+# see: https://github.com/conda/conda/issues/13451#issuecomment-1896097903
+export CONDA_PREFIX=$CONDA_ROOT
+
 # >>> conda initialize >>>
 # !! Contents within this block are managed by 'conda init' !!
 if test -f /opt/homebrew/anaconda3/bin/conda
