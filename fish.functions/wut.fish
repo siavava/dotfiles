bdaec1ba1bdbc8af58ec86f334b72c3fad701e14
siavava <amittaijoel@outlook.com>
2025-02-17 04:03:55 -0800

    (update): wut?

---
+++ fish.functions/wut.fish
@@ -0,0 +1,27 @@
+#!/usr/bin/env fish
+# -*- coding: utf-8; -*-
+
+# What does this do?
+# No one knows, which is the point.
+function wut
+
+  set_color red
+
+	printf "\nYou really fucked up huh.\n"
+
+  set_color -i
+  printf "But at least you did something.\n\n"
+  set_color normal
+
+  set_color red
+  printf "Right?\n"
+
+  printf "Right???\n\n"
+  
+  set_color green
+  printf "Okay then.\n\n"
+
+  set_color normal
+	return 0
+
+end
